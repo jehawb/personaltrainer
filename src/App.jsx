91f5c8ad931260c7b1127357bf2bf81025a23844
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2023-11-21 12:56:14 +0200

    buttonized navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 // import './App.css'
+import { Button } from "@mui/material";
 
 import { Link, Outlet } from "react-router-dom";
 
 export default function App() {
 
-  return (
-    <>
-        <div className="App">
-            <nav>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/trainings"}>Trainings</Link>
-                <Link to={"/customers"}>Customers</Link>
-            </nav>
-            <Outlet />
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="App">
+                <nav>
+                    <Link to={"/"}><Button>Home</Button></Link>
+                    <Link to={"/trainings"}><Button>Trainings</Button></Link>
+                    <Link to={"/customers"}><Button>Customers</Button></Link>
+                </nav>
+                <Outlet />
+            </div>
+        </>
+    )
 }
