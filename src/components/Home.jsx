7239e275b0c2b2ab81032c8a4f4df49a90e36274
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2023-11-23 11:49:02 +0200

    reset button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,11 @@
+import Reset from "./Reset";
+
 export default function Home() {
-    return (<h1>Welcome to Personal Trainer site! &#129318;</h1>);
+    return (
+        <>
+            <h1>Welcome to Personal Trainer site! &#129318;</h1>
+            <p>The application uses more or less public database with multiple users, if you want to reset the database, press the button below</p>
+            <Reset />
+        </>
+    );
 }
--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,2 +1,33 @@
 // TODO: Button to reset the database
 // Send a POST request to https://traineeapp.azurewebsites.net/reset
+
+import { Button } from '@mui/material';
+
+export default function Reset() {
+
+    // States
+
+
+    // Functions
+
+    const reset = () => {
+        fetch("http://traineeapp.azurewebsites.net/reset", { method: "POST" })
+            .then(response => {
+                if (response.ok) {
+                    console.log("Database reseted");
+                } else {
+                    alert("Something went wrong when trying to reset the database");
+                }
+            })
+            .catch(error => console.error(error));
+    }
+
+    // Return
+
+    return (
+        <>
+            <Button onClick={reset}>Reset Database</Button>
+        </>
+    );
+
+}
