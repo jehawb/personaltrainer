6cdacf447ccd69a87e066ea2a12f6118fc897110
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2023-12-02 12:35:04 +0200

    github pages deployment trials

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personaltrainer/',
+  // base: '/personaltrainer/',   // Required for github pages? Breaks all the navigation atleast
   plugins: [react()],
   test: {
     globals: true,
